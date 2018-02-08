406e005564cc0912e183187cc3fe1baf8096326e
erwang <eric.y.wang@nokia-sbell.com>
2018-02-08 15:51:12 +0800

    continue

--- src/avs_demo.c
+++ src/avs_demo.c
@@ -1,93 +1,26 @@
 #include <stdio.h>
-#include "msgqueue.h"
 
 pthread_t pid_thread;
 int avs_thread_quit_flag = 0;
-msg_queue_t* send_queue = 0;
-msg_queue_t* recv_queue = 0;
 
 void on_connected()
 {
-    msg_t* msg = build_create_downchannel_msg();
-    add_msg_to_send_queue(msg);
+    http2_send_create_downchannel_msg();
 }
 
-void on_downchannel_created()
+void on_downchannel_resp()
 {
-    msg_t* msg = build_sync_state_msg();
-    add_msg_to_send_queue(msg);
+    http2_send_sync_state_msg();
 }
 
-void on_timer()
-{
-    msg_t* msg = build_sync_state_msg();
-    add_msg_to_send_queue(msg);
-}
-
-void on_ask()
-{
-    msg_t* msg = build_ask_msg();
-    add_msg_to_send_queue(msg);
-}
-
-void on_reply(buf)
+void on_reply_req(buf)
 {
     playback(buf);
 }
 
-void do_keeplive()
+void on_timer()
 {
-    printf("nothing\n");
-}
-
-void handle(msg_t* msg)
-{
-    if (msg->type == 'connected')
-    {
-        on_connected();
-    }
-    if (msg->type == 'downchannel_created')
-    {
-        on_downchannel_created();
-    }
-    if (msg->type == 'ask')
-    {
-        on_ask();
-    }
-    if (msg->type == 'keeplive')
-    {
-        do_keeplive();
-    }
-    if (msg->type == 'reply')
-    {
-        on_reply();
-    }
-    if (msg->type == 'timer')
-    {
-        on_timer();
-    }
-}
-
-void avs_thread()
-{
-    while (avs_thread_quit_flag == 0)
-    {
-        http2_loop();
-        if (msg_t* msg = get_msg_from_send_queue())
-        {
-            http2_send_msg(msg);
-            free_msg(msg);
-        }
-        if (msg_t* msg = get_msg_from_recv_queue())
-        {
-            handle(msg);
-            free_msg(msg);
-        }
-        if (get_timer())
-        {
-            on_timer();
-        }
-    }
+    http2_send_sync_state_msg();
 }
 
 void http2_cb(char* head, char* body)
@@ -95,28 +28,24 @@ void http2_cb(char* head, char* body)
     msg_t* msg;
     if (head == 'connected')
     {
-        msg->type = 'connected';
+        on_connected();
     }
     if (head == 'sync')
     {
-        msg->type = 'sync';
+        on_sync_resp();
     }
     if (head == 'downchannel')
     {
-        msg->type = 'downchannel';
+        on_downchannel_created();
     }
     if (head == 'data')
     {
-        msg->type = 'reply';
-        msg->data = body;
+        on_reply();
     }
-    add_msg_to_recv_queue(msg);
 }
 
 void start_connection()
 {
-    create_send_queue();
-    create_recv_queue();
     create_http2(http2_cb);
     start_thread(avs_thread);
 }
@@ -126,8 +55,6 @@ void stop_connection()
     avs_thread_quit_flag = 1;
     wait_for_safe_close();
     destroy_http2();
-    destroy_send_queue();
-    destroy_recv_queue();
 }
 
 void start_thread()
@@ -140,29 +67,32 @@ void wait_for_safe_close()
     pthread_join(pid_thread, NULL);
 }
 
-int get_timer()
+void timer_process()
 {
-    return 0;
+    on_timer();
+}
+
+void avs_thread()
+{
+    while (avs_thread_quit_flag == 0)
+    {
+        http2_process();
+        timer_process();
+    }
 }
 
 void test_1()
 {
     int len;
     char* buf = load_pcm('1.pcm', &len);
-    msg_t* msg = build_ask_msg(buf, len);
-    add_msg_to_send_queue(msg);
+    http2_send_ask_msg(buf, len);
 }
 
 void test_2()
 {
     int len;
     char* buf = load_pcm('2.pcm', &len);
-    msg_t* msg = build_ask_msg(buf, len);
-    add_msg_to_send_queue(msg);
-}
-
-void playback(char* buf, int len)
-{
+    http2_send_ask_msg(buf, len);
 }
 
 int main(int argc, char** argv)
--- src/http2.c
+++ src/http2.c
@@ -1,4 +1,10 @@
+#include <stdio.h>
 
+SSL_CTX *ssl_ctx;
+SSL *ssl;
+nghttp2_session *session;
+nghttp2_session_callbacks *callbacks;
+int fd, flags, val;
 
 static ssize_t send_callback(nghttp2_session *session, const uint8_t *data,
                              size_t length, int flags, void *user_data)
@@ -60,42 +66,7 @@ static ssize_t recv_callback(nghttp2_session *session, uint8_t *buf,
     return rv;
 }
 
-static int on_frame_send_callback(nghttp2_session *session,
-                                  const nghttp2_frame *frame, void *user_data)
-{
-    size_t i;
-    (void)user_data;
-    nghttp2_nv* nva;
-
-    switch (frame->hd.type)
-    {
-        case NGHTTP2_HEADERS:
-            nva = frame->headers.nva;
-            printf("[INFO] C ----------------------------> S (HEADERS)\n");
-
-            for (i = 0; i < frame->headers.nvlen; ++i)
-            {
-                fwrite(nva[i].name, 1, nva[i].namelen, stdout);
-                printf(": ");
-                fwrite(nva[i].value, 1, nva[i].valuelen, stdout);
-                printf("\n");
-            }
-
-            break;
-
-        case NGHTTP2_RST_STREAM:
-            printf("[INFO] C ----------------------------> S (RST_STREAM)\n");
-            break;
-
-        case NGHTTP2_GOAWAY:
-            printf("[INFO] C ----------------------------> S (GOAWAY)\n");
-            break;
-    }
-
-    return 0;
-}
-
-static int on_frame_recv_callback(nghttp2_session *session,
+static int frame_recv_callback(nghttp2_session *session,
                                   const nghttp2_frame *frame, void *user_data)
 {
     size_t i;
@@ -139,7 +110,7 @@ static int on_frame_recv_callback(nghttp2_session *session,
     return 0;
 }
 
-static int on_data_chunk_recv_callback(nghttp2_session *session, uint8_t flags,
+static int data_chunk_recv_callback(nghttp2_session *session, uint8_t flags,
                                        int32_t stream_id, const uint8_t *data,
                                        size_t len, void *user_data)
 {
@@ -158,7 +129,7 @@ static int on_data_chunk_recv_callback(nghttp2_session *session, uint8_t flags,
     return 0;
 }
 
-static int on_stream_close_callback(nghttp2_session *session, int32_t stream_id,
+static int stream_close_callback(nghttp2_session *session, int32_t stream_id,
                                     uint32_t error_code, void *user_data)
 {
     (void)error_code;
@@ -173,92 +144,19 @@ static int on_stream_close_callback(nghttp2_session *session, int32_t stream_id,
     return 0;
 }
 
-static int on_header_callback(nghttp2_session *session, const nghttp2_frame *frame, const uint8_t *name, size_t namelen, const uint8_t *value, size_t valuelen, uint8_t flags, void *user_data)
+static int header_callback(nghttp2_session *session, const nghttp2_frame *frame, const uint8_t *name, size_t namelen, const uint8_t *value, size_t valuelen, uint8_t flags, void *user_data)
 {
     verbose_header(session, frame, name, namelen, value, valuelen, flags, user_data);
     return 0;
 }
 
-static void setup_nghttp2_callbacks(nghttp2_session_callbacks *callbacks)
+static int select_next_proto_cb(SSL *ssl, unsigned char **out, unsigned char *outlen, const unsigned char *in, unsigned int inlen, void *arg)
 {
-    nghttp2_session_callbacks_set_send_callback(callbacks, send_callback);
-    nghttp2_session_callbacks_set_recv_callback(callbacks, recv_callback);
-    nghttp2_session_callbacks_set_on_frame_send_callback(callbacks, on_frame_send_callback);
-    nghttp2_session_callbacks_set_on_frame_recv_callback(callbacks, on_frame_recv_callback);
-    nghttp2_session_callbacks_set_on_data_chunk_recv_callback(callbacks, on_data_chunk_recv_callback);
-    nghttp2_session_callbacks_set_on_stream_close_callback(callbacks, on_stream_close_callback);
-    nghttp2_session_callbacks_set_on_header_callback(callbacks, on_header_callback);
-
-        /* nghttp2_on_frame_recv_callback */
-        nghttp2_session_callbacks_set_on_frame_recv_callback
-        (callbacks, on_frame_recv);
-        /* nghttp2_on_invalid_frame_recv_callback */
-        nghttp2_session_callbacks_set_on_invalid_frame_recv_callback
-        (callbacks, on_invalid_frame_recv);
-        /* nghttp2_on_data_chunk_recv_callback */
-        nghttp2_session_callbacks_set_on_data_chunk_recv_callback
-        (callbacks, on_data_chunk_recv);
-        /* nghttp2_before_frame_send_callback */
-        nghttp2_session_callbacks_set_before_frame_send_callback
-        (callbacks, before_frame_send);
-        /* nghttp2_on_frame_send_callback */
-        nghttp2_session_callbacks_set_on_frame_send_callback
-        (callbacks, on_frame_send);
-        /* nghttp2_on_frame_not_send_callback */
-        nghttp2_session_callbacks_set_on_frame_not_send_callback
-        (callbacks, on_frame_not_send);
-        /* nghttp2_on_stream_close_callback */
-        nghttp2_session_callbacks_set_on_stream_close_callback
-        (callbacks, on_stream_close);
-        /* nghttp2_on_begin_headers_callback */
-        nghttp2_session_callbacks_set_on_begin_headers_callback
-        (callbacks, on_begin_headers);
-        /* nghttp2_on_header_callback */
-        nghttp2_session_callbacks_set_on_header_callback(callbacks, on_header);
-
-        nghttp2_session_callbacks_set_error_callback(callbacks, error_callback);    
-}
-
-
-static int select_next_proto_cb(SSL *ssl, unsigned char **out,
-                                unsigned char *outlen, const unsigned char *in,
-                                unsigned int inlen, void *arg)
-{
-    int rv;
-    (void)ssl;
-    (void)arg;
-    rv = nghttp2_select_next_protocol(out, outlen, in, inlen);
-
-    if (rv <= 0)
-        die("Server did not advertise HTTP/2 protocol");
-
+    nghttp2_select_next_protocol(out, outlen, in, inlen);
     return SSL_TLSEXT_ERR_OK;
 }
 
-static void init_ssl_ctx(SSL_CTX *ssl_ctx)
-{
-    SSL_CTX_set_options(ssl_ctx, SSL_OP_ALL | SSL_OP_NO_SSLv2);
-    SSL_CTX_set_mode(ssl_ctx, SSL_MODE_AUTO_RETRY);
-    SSL_CTX_set_mode(ssl_ctx, SSL_MODE_RELEASE_BUFFERS);
-    SSL_CTX_set_next_proto_select_cb(ssl_ctx, select_next_proto_cb, NULL);
-}
-
-static void ssl_handshake(SSL *ssl, int fd)
-{
-    int rv;
-
-    if (SSL_set_fd(ssl, fd) == 0)
-        dief("SSL_set_fd", ERR_error_string(ERR_get_error(), NULL));
-
-    ERR_clear_error();
-    rv = SSL_connect(ssl);
-
-    if (rv <= 0)
-        dief("SSL_connect", ERR_error_string(ERR_get_error(), NULL));
-}
-
-
-void http2_loop()
+void http2_run()
 {
     fd_set fdset;
     struct timeval tv;
@@ -314,64 +212,42 @@ void create_http2(http2_cb_t cb)
 {
     SSL_load_error_strings();
     SSL_library_init();
-    fd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
-        while ((rv = connect(fd, rp->ai_addr, rp->ai_addrlen)) == -1 &&
-                errno == EINTR)
     ssl_ctx = SSL_CTX_new(SSLv23_client_method());
-
-    if (ssl_ctx == NULL)
-        dief("SSL_CTX_new", ERR_error_string(ERR_get_error(), NULL));
-
-    init_ssl_ctx(ssl_ctx);
+    SSL_CTX_set_options(ssl_ctx, SSL_OP_ALL | SSL_OP_NO_SSLv2);
+    SSL_CTX_set_mode(ssl_ctx, SSL_MODE_AUTO_RETRY);
+    SSL_CTX_set_mode(ssl_ctx, SSL_MODE_RELEASE_BUFFERS);
+    SSL_CTX_set_next_proto_select_cb(ssl_ctx, select_next_proto_cb, 0);    
     ssl = SSL_new(ssl_ctx);
+    
+    fd = socket(AF_INET, SOCK_STREAM, 0);
+    connect(fd, rp->ai_addr, rp->ai_addrlen);
+    SSL_set_fd(ssl, fd);
+    SSL_connect(ssl);
 
-    if (ssl == NULL)
-        dief("SSL_new", ERR_error_string(ERR_get_error(), NULL));
+    flags = fcntl(fd, F_GETFL, 0);
+    fcntl(fd, F_SETFL, flags | O_NONBLOCK);
+    val = 1;
+    setsockopt(fd, IPPROTO_TCP, TCP_NODELAY, &val, (socklen_t)sizeof(val));
 
-    /* To simplify the program, we perform SSL/TLS handshake in blocking
-       I/O. */
-    ssl_handshake(ssl, fd);
-    connection.ssl = ssl;
-    connection.want_io = IO_NONE;
-    /* Here make file descriptor non-block */
-    make_non_block(fd);
-    set_tcp_nodelay(fd);
-    printf("[INFO] SSL/TLS handshake completed\n");
-    rv = nghttp2_session_callbacks_new(&callbacks);
-
-    if (rv != 0)
-        diec("nghttp2_session_callbacks_new", rv);
-
+    nghttp2_session_callbacks_new(&callbacks);
     nghttp2_session_callbacks_set_send_callback(callbacks, send_callback);
     nghttp2_session_callbacks_set_recv_callback(callbacks, recv_callback);
-    nghttp2_session_callbacks_set_on_frame_send_callback(callbacks, on_frame_send_callback);
-    nghttp2_session_callbacks_set_on_frame_recv_callback(callbacks, on_frame_recv_callback);
-    nghttp2_session_callbacks_set_on_data_chunk_recv_callback(callbacks, on_data_chunk_recv_callback);
-    nghttp2_session_callbacks_set_on_stream_close_callback(callbacks, on_stream_close_callback);
-    nghttp2_session_callbacks_set_on_header_callback(callbacks, on_header_callback);
-
-    rv = nghttp2_session_client_new(&connection.session, callbacks, &connection);
+    nghttp2_session_callbacks_set_on_frame_recv_callback(callbacks, frame_recv_callback);
+    nghttp2_session_callbacks_set_on_data_chunk_recv_callback(callbacks, data_chunk_recv_callback);
+    nghttp2_session_callbacks_set_on_stream_close_callback(callbacks, stream_close_callback);
+    nghttp2_session_callbacks_set_on_header_callback(callbacks, header_callback);
+    nghttp2_session_client_new(&session, callbacks, 0);
     nghttp2_session_callbacks_del(callbacks);
 
-    if (rv != 0)
-        diec("nghttp2_session_client_new", rv);
-
-    rv = nghttp2_submit_settings(connection.session, NGHTTP2_FLAG_NONE, NULL, 0);
-
-    if (rv != 0)
-        diec("nghttp2_submit_settings", rv);
-        
-
-
+    nghttp2_submit_settings(session, NGHTTP2_FLAG_NONE, NULL, 0);
 }
 
 void destroy_http2()
 {
-    nghttp2_session_del(connection.session);
+    nghttp2_session_del(session);
     SSL_shutdown(ssl);
     SSL_free(ssl);
     SSL_CTX_free(ssl_ctx);
     shutdown(fd, SHUT_WR);
     close(fd);
 }
-
